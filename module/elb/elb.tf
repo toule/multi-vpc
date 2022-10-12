913cae2fcb2413bfaf0408f27d3847afce68a647
Ray.H.Li <toule@RayHLi-MacBookPro.local>
2022-10-12 11:51:08 +0900

    add elb module

--- module/elb/elb.tf
+++ module/elb/elb.tf
@@ -1,17 +1,18 @@
 resource "aws_lb" "test" {
+  count = var.create_elb == true ? 1:0
   name               = var.elb_name
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [for subnet in aws_subnet.public : subnet.id]
+  internal           = var.enable_internal
+  load_balancer_type = var.elb_type
+  security_groups    = var.security_groups_id
+  subnets            = var.subnets_id
 
-  enable_deletion_protection = true
+  enable_deletion_protection = var.enable_protection
 
-  access_logs {
-    bucket  = aws_s3_bucket.lb_logs.bucket
-    prefix  = "test-lb"
-    enabled = true
-  }
+ #access_logs {
+   #bucket  = aws_s3_bucket.lb_logs.bucket
+   #prefix  = "test-lb"
+   #enabled = true
+#}
 
   tags = {
     Environment = "production"
