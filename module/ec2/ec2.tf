97c7f1a1793bb63fbadd503a654ad5e0dfe91b2c
Ray.H.Li <toule@RayHLi-MacBookPro.local>
2022-10-20 16:05:02 +0900

    add public IP

--- module/ec2/ec2.tf
+++ module/ec2/ec2.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "ec2" {
 	subnet_id				= var.subnet_id
 	key_name				= var.key_name
 	vpc_security_group_ids	= var.sg_id
-    associate_public_ip_address = var.associate_public_ip_address
+	associate_public_ip_address = var.associate_public_ip_address
 
 	tags = merge(
         {"Name"=var.ec2_name},
--- stage/source/main.tf
+++ stage/source/main.tf
@@ -81,7 +81,7 @@ module "bastion" {
     source = "github.com/toule/terraform-rayhli/module/ec2"
     create_ec2 = true
     ec2_name = "bastion-dev-instance"
-
+	
 	associate_public_ip_address = true
     user_data = local.user_data
     ami = "ami-01d87646ef267ccd7"
